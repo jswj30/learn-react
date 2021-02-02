6ee44dcd7ba9cc798268d367ae9a3c375f099097
jswj30 <jswj30@naver.com>
2021-02-02 14:19:17 +0900

    03

---
+++ 03/hello-react/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import MyComponent from "./MyComponent";
+import Counter from "./Counter";
+
+const App = () => {
+  return <Counter />;
+};
+
+// const App = () => {
+//   return (
+//     <MyComponent name="React" favoriteNumber={1}>
+//       리액트
+//     </MyComponent>
+//   );
+// };
+
+export default App;
