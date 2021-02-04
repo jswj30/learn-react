2f60b3940f10f28e228df1380b0dcd3bf7f8f3c3
jswj30 <jswj30@naver.com>
2021-02-04 19:30:55 +0900

    5.1

---
+++ 05/hello-react/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import ValidationSample from "./ValidationSample";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ValidationSample />
+      </div>
+    );
+  }
+}
+
+export default App;
