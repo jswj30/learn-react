25474965b433aae1fce3bac942f49612b79e1968
jswj30 <jswj30@naver.com>
2021-02-05 12:42:47 +0900

    5.3.2

--- 05/hello-react/src/App.js
+++ 05/hello-react/src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from "react";
 import ValidationSample from "./ValidationSample";
+import ScrollBox from "./ScrollBox";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <ValidationSample />
+        <ScrollBox />
       </div>
     );
   }
 }
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <ValidationSample />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
