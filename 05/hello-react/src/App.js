f6bdadd87976529a39ab8795d6313aa5af36c028
jswj30 <jswj30@naver.com>
2021-02-06 15:40:43 +0900

    05

--- 05/hello-react/src/App.js
+++ 05/hello-react/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
-import ValidationSample from "./ValidationSample";
+// import ValidationSample from "./ValidationSample";
 import ScrollBox from "./ScrollBox";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <ScrollBox />
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
       </div>
     );
   }
