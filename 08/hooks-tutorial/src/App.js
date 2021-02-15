2577c72c471a821284aa95bc6cd248385cd40b93
jswj30 <jswj30@naver.com>
2021-02-15 15:27:33 +0900

    8.1

---
+++ 08/hooks-tutorial/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+// import Counter from "./Counter";
+import Info from "./Info";
+
+const App = () => {
+  // return <Counter />;
+  return <Info />;
+};
+
+export default App;
