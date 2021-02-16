7331c160195d2cd0402d409e2801b51fef67e799
jswj30 <jswj30@naver.com>
2021-02-16 12:58:46 +0900

    8.3

--- 08/hooks-tutorial/src/App.js
+++ 08/hooks-tutorial/src/App.js
@@ -1,23 +1,41 @@
-import React, { useState } from "react";
-// import Counter from "./Counter";
+import React from "react";
 import Info from "./Info";
 
 const App = () => {
-  const [visible, setVisible] = useState(false);
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? "숨기기" : "보이기"}
-      </button>
-      <hr />
-      {visible && <Info />}
-    </div>
-  );
-  // return <Counter />;
+  return <Info />;
 };
 
 export default App;
+
+// import React from "react";
+// import Counter from "./Counter";
+
+// const App = () => {
+//   return <Counter />;
+// };
+
+// export default App;
+
+// import React, { useState } from "react";
+// // import Counter from "./Counter";
+// import Info from "./Info";
+
+// const App = () => {
+//   const [visible, setVisible] = useState(false);
+//   return (
+//     <div>
+//       <button
+//         onClick={() => {
+//           setVisible(!visible);
+//         }}
+//       >
+//         {visible ? "숨기기" : "보이기"}
+//       </button>
+//       <hr />
+//       {visible && <Info />}
+//     </div>
+//   );
+//   // return <Counter />;
+// };
+
+// export default App;
