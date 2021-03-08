ced3168d09585262e13eaa0a385dde53f42ad6b2
jswj30 <jswj30@naver.com>
2021-03-08 14:29:29 +0900

    10.2

--- 10/todo-app/src/App.js
+++ 10/todo-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import TodoTemplate from './component/TodoTemplate';
 import TodoInsert from './component/TodoInsert';
+import TodoList from './component/TodoList';
 
 const App = () => {
   return (
     <TodoTemplate>
       <TodoInsert />
+      <TodoList />
     </TodoTemplate>
   );
 };
